10031ce6e9097a2104ed0768415edd02a994ae7d
phuong nguyen <phuong.nguyen.itvn@gmail.com>
2014-06-29 21:24:35 +0700

    update

--- source/protected/views/site/index.php
+++ source/protected/views/site/index.php
@@ -184,53 +184,5 @@ Yii::app()->getClientScript()->registerScriptFile("$themeUrl/js/jquery.prettyPho
 		</div>
 	</div>
 	<!-- clients -->
-	<section class="clients">
-		<div class="holder">
-			<h3>We have done work for prominent, international companies. Would you like to work with us?</h3>
-			<p>Listing them all would take a little time so we thought weâ€™d pull a few well known names from the list. We also have testimonials from customers regarding positive experiences with our service.</p>
-			<!-- sponsors -->
-			<ul class="sponsors">
-				<li>
-					<a href="#">
-						<span>
-							<img src="<?php echo $themeUrl; ?>/images/img4.png" alt="image description" />
-							<img class="hover" src="<?php echo $themeUrl; ?>/images/img4-hover.png" alt="image description" />
-						</span>
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						<span>
-							<img src="<?php echo $themeUrl; ?>/images/img5.png" alt="image description" />
-							<img class="hover" src="<?php echo $themeUrl; ?>/images/img5-hover.png" alt="image description" />
-						</span>
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						<span>
-							<img src="<?php echo $themeUrl; ?>/images/img6.png" alt="image description" />
-							<img class="hover" src="<?php echo $themeUrl; ?>/images/img6-hover.png" alt="image description" />
-						</span>
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						<span>
-							<img src="<?php echo $themeUrl; ?>/images/img7.png" alt="image description" />
-							<img class="hover" src="<?php echo $themeUrl; ?>/images/img7-hover.png" alt="image description" />
-						</span>
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						<span>
-							<img src="<?php echo $themeUrl; ?>/images/img8.png" alt="image description" />
-							<img class="hover" src="<?php echo $themeUrl; ?>/images/img8-hover.png" alt="image description" />
-						</span>
-					</a>
-				</li>
-			</ul>
-		</div>
-	</section>
+	<?php $this->widget('application.widgets.partner.PartnerWidget')?>
 </div>
