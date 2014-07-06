dbc966cfc5642a04f09162eaedd90636b3b0a929
phuong nguyen <phuong.nguyen.itvn@gmail.com>
2014-07-06 17:23:20 +0700

    update

---
+++ source/protected/controllers/PageController.php
@@ -0,0 +1,12 @@
+<?php
+class PageController extends FrontendController
+{
+	public $layout='//layouts/body';
+	public function actionView()
+	{
+		$id = Yii::app()->request->getParam('id');
+		$page = FrontendPagesModel::model()->findByPk($id);
+		$this->pageTitle = $page->title;
+		$this->render('view');
+	}
+}
---
+++ source/protected/views/layouts/body.php
@@ -0,0 +1,20 @@
+<?php
+$themeUrl = Yii::app()->theme->baseUrl;
+?>
+<?php $this->beginContent('//layouts/main'); ?>
+<div class="content-panel">
+	<div class="page-title">
+		<h2><?php echo $this->pageTitle;?></h2>
+		<!-- breadcrumbs -->
+		<ul class="breadcrumbs">
+			<li><a href="./index.html">Trang chủ</a></li>
+			<li>/</li>
+			<li>Blog</li>
+		</ul>
+	</div>
+</div>
+<!-- content -->
+<div class="container">
+<div id="content"><div class="c1"><?php echo $content; ?></div></div>
+</div>
+<?php $this->endContent(); ?>
