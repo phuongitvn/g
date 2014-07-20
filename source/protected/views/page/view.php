60b154a376dac8932e2a50eb61a976dcb79dc1a9
phuong nguyen <phuong.nguyen.itvn@gmail.com>
2014-07-20 21:23:49 +0700

    update

--- source/protected/views/page/view.php
+++ source/protected/views/page/view.php
@@ -1,5 +1,6 @@
 <div class="tabs-vertical">
 <?php if($childPages):?>
+<div style="overflow: hidden;float: left;height: 100%;">
 <ul class="tabset">
 <?php foreach ($childPages as $key => $item):
 	$link = Yii::app()->controller->createUrlPage($item->id);
@@ -8,6 +9,7 @@
 	<li class="<?php echo $active;?>"><a href="<?php echo $link;?>"><?php echo $item->title;?></a></li>
 <?php endforeach;?>
 </ul>
+</div>
 <?php endif;?>
 <h3><?php echo $page->title?></h3>
 <div class="page_content"><?php echo $page->fulltext;?></div>
